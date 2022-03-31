9d77b9a0cf8b2091e3593c869d2f38f3ac57bac9
Andy Caseres <andycaseres2@gmail.com>
2022-03-31 14:07:05 -0500

    Add Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./Components/Pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Atrium Physio Therapy</h1>
+      <Home />
     </div>
   );
 }
