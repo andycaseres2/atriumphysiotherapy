3cc2e7a3926e14912e2b99029d2a23186fc21e80
Damian <58891112+damianroiz@users.noreply.github.com>
2022-04-03 17:50:10 -0400

    added global routes to nav item links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Home from "./Pages/Home/Home"
+import About from "./Pages/About/About"
+import { Route} from "wouter"
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+    
+      <Route path="/" component={Home}/>
+      <Route path="/About" component={About}/>
+
     </div>
   );
 }
