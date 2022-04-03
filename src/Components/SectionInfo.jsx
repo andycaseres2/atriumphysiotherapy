915dc9ff267bc613fc19540acd76a3d4aa1c6b5d
Andy Caseres <andycaseres2@gmail.com>
2022-04-02 23:48:03 -0500

    add icons sectionInfo and styles

--- src/Components/SectionInfo.jsx
+++ src/Components/SectionInfo.jsx
@@ -1,43 +1,57 @@
 import React from 'react'
-import logo from "../assets/images/atriumLogo.png"
-import "./sectioninfo.css"
-import { GoLocation } from 'react-icons/go';
-import { HiOutlineMail } from 'react-icons/hi';
+import logo from '../assets/images/atriumLogo.png'
+import './sectioninfo.css'
+import { ImLocation2 } from 'react-icons/im'
+import { GrMail } from 'react-icons/gr'
+import { BsFillTelephoneFill } from 'react-icons/bs'
+import { FaFax } from 'react-icons/fa'
+import { GoAlert } from 'react-icons/go'
 
 const SectionInfo = () => {
   return (
     //Add logo img inside the following div
     <div className='sectioninfo'>
-        <div className="logo-container">
-            <img className='logo' src={logo} alt="logo" />
-        </div>
-        <div className="contact-info">
-            <div className="address">
-            <div className="icon">
-            <GoLocation/>
+      <div className='logo-container'>
+        <a href='/'>
+          <img className='logo' src={logo} alt='logo' />
+        </a>
+      </div>
+      <div className='containerinfo'>
+        <div className='contact-info'>
+          <div className='address'>
+            <div className='icon'>
+              <ImLocation2 />
             </div>
             <p>Address: #140 - 340 Midpark Way SE, T2X 1P1</p>
+          </div>
+          <div className='email'>
+            <div className='icon'>
+              <GrMail />
             </div>
-            <div className="email">
-                <div className="icon">
-                <HiOutlineMail/>
-                </div>
-                <p>Email: admin@atriumphysiotherapy.com</p>
+            <p>Email: admin@atriumphysiotherapy.com</p>
+          </div>
+          <div className='phone'>
+            <div className='icon'>
+              <BsFillTelephoneFill />
             </div>
-            <div className="phone">
-            <div className="icon">
-                <BsFillTelephoneFill/>
-                </div>
-                <p>Phone: 403.255.4461</p>
+            <p>Phone: 403.255.4461</p>
+          </div>
+          <div className='fax'>
+            <div className='icon'>
+              <FaFax />
             </div>
-            <div className="fax">
-            <div className="icon">
-                <FaFax/>
-                </div>
-                <p>Fax: 403.259.8776 </p>
-            </div>
+            <p>Fax: 403.259.8776 </p>
+          </div>
         </div>
-        <div className="covidbtn"></div>
+      </div>
+      <div className='covidbtn'>
+        <button className='btn'>
+          <span>
+            <GoAlert />
+          </span>
+          COVID-19 Alert
+        </button>
+      </div>
     </div>
   )
 }
