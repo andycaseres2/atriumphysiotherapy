2ec054a5a5e6418e3fe8ebc73eb8ecdf34d9ebd8
Damian <58891112+damianroiz@users.noreply.github.com>
2022-04-03 17:47:08 -0400

    navbar added to home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import SectionInfo from "../../Components/SectionInfo"
+import { Navbar } from "../../Components/Navbar";
 
 const Home = () => {
   return <div>
     <SectionInfo/>
+    <Navbar/>
   </div>;
 };
 
