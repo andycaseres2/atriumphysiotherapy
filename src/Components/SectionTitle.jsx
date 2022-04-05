979203765b1f6abe08311e05d1f9f2762aed3713
AndyCaseres <79545035+andycaseres2@users.noreply.github.com>
2022-04-05 10:19:36 -0500

    Add files via upload

---
+++ src/Components/SectionTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sectiontitle.css";
+
+const SectionTitle = () => {
+  return (
+    <section className="About">
+      <h1 className="about-title">About</h1>
+    </section>
+  );
+};
+
+export default SectionTitle;
