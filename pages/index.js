33cddb71271d5a6ae95e5cfefd74860c5f8ae945
Damian <58891112+damianroiz@users.noreply.github.com>
2022-04-10 12:32:21 -0400

    GraphQL Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
+
 const posts = [
   {
     title: '10 Back stretches you need', excerpt: 'learn these effective back stretching movements'
@@ -16,15 +18,16 @@ export default function home (){
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+        <div className='lg:col-span-8 col-span-1'>
+        {posts.map((post) => <PostCard post={post} key={post.title} />)}
+        </div>   
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
-
-
     </div>
   )
 }
