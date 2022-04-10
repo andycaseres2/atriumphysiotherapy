0beafe9c374a8d2d9beae211dbbfa61243e2b2b0
Damian <58891112+damianroiz@users.noreply.github.com>
2022-04-10 00:34:10 -0400

    setting up the code on index.js

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+const post = [
+  {
+    title: '10 Back stretches you need', excerpt: 'learn these effective back stretching movements'
+  },
+  {
+    title: 'Learn to sleep better', excerpt: 'Effective ways for a better sleep quality every night'
+  }
+];
+
+
+
+
+export default function home (){
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        {post.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+      </div>
+
+
+    </div>
+  )
+}
+
