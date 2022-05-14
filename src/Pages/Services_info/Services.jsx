6c5e4ecfdc04c643f40cc672460794832a5ab941
Andy Caseres <andycaseres2@gmail.com>
2022-05-13 21:01:17 -0500

    add inf data

--- src/Pages/Services_info/Services.jsx
+++ src/Pages/Services_info/Services.jsx
@@ -1,42 +1,54 @@
-import React , {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import SectionInfo from "../../Components/SectionInfo";
 import { Navbar } from "../../Components/Navbar";
-import Footer from "../../Components/Footer";
+// import Footer from "../../Components/Footer";
 import { ServicesItems } from "./ServicesItems";
-import SectionTitle from "../../Components/SectionTitle"
-import {servicesdata} from "../Services_info/services_data"
-import {Grid , Box} from "@chakra-ui/react"
+import SectionTitle from "../../Components/SectionTitle";
+import { servicesdata } from "../Services_info/services_data";
+import { Grid, Box } from "@chakra-ui/react";
 import "./services.css";
-import {Services_Content} from "./Services_Content"
+import { Services_Content } from "./Services_Content";
 
 const Services = () => {
-
-  const [current , setCurrent] = useState()
-
+  const [current, setCurrent] = useState();
 
   return (
     <>
       <SectionInfo />
       <Navbar />
-      <SectionTitle title="Services"/>
+      <SectionTitle title="Services" />
       <section className="services_section">
-      <h4 className="subtitle_services">SERVICES</h4>
-      <h2 className="title_services">Clinic Services</h2>
-      <p className="parrafo_services">Information pertaining to the high quality services offered by our clinic</p>
+        <h4 className="subtitle_services">SERVICES</h4>
+        <h2 className="title_services">Clinic Services</h2>
+        <p className="parrafo_services">
+          Information pertaining to the high quality services offered by our
+          clinic
+        </p>
       </section>
       <div className="grid_services">
-        <Grid className="grid_services" templateColumns='repeat(5, 1fr)' gap={6} mt={2}>
-      {servicesdata.map(item =>
-      <ServicesItems  key={item.id} item={item} setCurrent={setCurrent} />
-      )}
-      </Grid>
+        <Grid
+          className="grid_services"
+          templateColumns="repeat(5, 1fr)"
+          gap={6}
+          mt={2}
+        >
+          {servicesdata.map((item) => (
+            <ServicesItems key={item.id} item={item} setCurrent={setCurrent} />
+          ))}
+        </Grid>
       </div>
-    <div>
-   <Box>
-        {current && servicesdata.map(item => item.id === current && (<Services_Content key={item.id} {...item}/>))}
-   </Box>
-    </div>
-      <Footer />
+      <div>
+        <Box>
+          {current &&
+            servicesdata.map(
+              (item) =>
+                item.id === current && (
+                  <Services_Content key={item.id} {...item} />
+                )
+            )}
+        </Box>
+      </div>
+      {/* <Footer /> */}
     </>
   );
 };
--- src/Pages/Services_info/Services_Content.jsx
+++ src/Pages/Services_info/Services_Content.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import {Stack , Text} from "@chakra-ui/react"
+import React from "react";
+import { Stack, Text } from "@chakra-ui/react";
 
 export const Services_Content = (item) => {
   return (
     <Stack>
-        <Text>{item.title}</Text>
+      <Text>{item.title}</Text>
+      <Text>{item.subtitle}</Text>
+      <Text>{item.descriptionp1}</Text>
+      <Text>{item.descriptionp2}</Text>
+      <Text>{item.descriptionp3}</Text>
+      <Text>{item.descriptionp4}</Text>
+      <Text>{item.descriptionp5}</Text>
+      <Text>{item.descriptionp6}</Text>
     </Stack>
-
-  )
-}
-
+  );
+};
