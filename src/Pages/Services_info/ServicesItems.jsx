89f0208ede7c00526fb7c06096471b275a628274
Andy Caseres <andycaseres2@gmail.com>
2022-05-15 22:06:04 -0500

    Add inf at data_services

--- src/Pages/Services_info/ServicesItems.jsx
+++ src/Pages/Services_info/ServicesItems.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import "./servicesitems.css";
 
-export const ServicesItems = ({item , setCurrent}) => {
+export const ServicesItems = ({ item, setCurrent }) => {
   return (
-      <Grid onClick={() => setCurrent(item.id)}
-        className="services"
-        templateRows='repeat(1, 1fr)'>
-        <GridItem className="servicesitems" w="100%" h="100" bg="blue">
-         <h4>{item.title}</h4>
-        </GridItem>
-      </Grid>
+    <Grid
+      onClick={() => setCurrent(item.id)}
+      className="services"
+      templateRows="repeat(1, 1fr)"
+    >
+      <GridItem className="servicesitems" w="99%" h="100" bg="blue">
+        <h4>{item.title}</h4>
+      </GridItem>
+    </Grid>
   );
 };
--- src/Pages/Services_info/services_data.js
+++ src/Pages/Services_info/services_data.js
@@ -30,44 +30,145 @@ export const servicesdata = [
   },
   {
     id: 3,
-    title: "5655",
-    image: "../../assets/images/servicesimg/physiotherapy.jpg",
-    subtitle:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, in. Eaque officiis amet eos perferendis porro placeat alias tempore. Blanditiis dolorum accusantium libero voluptate beatae delectus expedita, debitis eligendi totam.",
+    title: "Acupuncture",
+    image: "https://atriumphysiotherapy.com/images/acupuncture.jpg",
+    descriptionp1:
+      "Acupuncture is an ancient form of Chinese medicine involving the insertion of ultra-fine needles into specific points of the skin. These points of energy are located along 14 meridians, or channels, that cover the entire body. Using modern techniques, contemporary medical acupuncture is able to precisely stimulate nerves to modulate pain and other dysfunctions..",
   },
   {
     id: 4,
-    title: "7687878",
-    image: "../../assets/images/servicesimg/physiotherapy.jpg",
-    subtitle:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium eaque, quisquam consequuntur magnam neque quod eveniet numquam saepe iure at, ipsa fugit, odio impedit hic animi sapiente et consectetur quae!",
+    title: "Graston Technique ®",
+    image: "https://atriumphysiotherapy.com/images/Graston.jpg",
+    descriptionp1:
+      "Graston technique is a form of instrument-assisted or augmented soft tissue mobilization (ASTM) that enables practitioners to improve scar tissue, fascial restrictions and range of motion. The theory behind this IASTM technique is that by using a tool to introduce microtrauma into an area of excessive scarring and/or soft tissue fibrosis, an inflammatory response will occur. In a 2017 Journal of Exercise Rehabilitation study, the authors describe that “Such inflammation restarts the healing process by removing the scar tissue and releasing adhesions, while also increasing blood and nutrient supply to the injured area and migration of fibroblasts.” (1b)",
+    descriptionp2:
+      "The use of tools helps to get to the root of the problem, but it’s also intended to reduce stress on the therapist’s hands. Graston technique allows a practitioner to get deep into the problematic tissue yet be sensitive to a patient’s level of pain tolerance. As the instruments are moved over the affected area and come in contact with adhesions, they help to break up scar tissue dysfunction and restrictions of the fascia.",
   },
+
   {
     id: 5,
-    title: "Physiohhtgtg",
-    image: "../../assets/images/servicesimg/physiotherapy.jpg",
-    subtitle:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non quisquam dolores nesciunt molestiae explicabo, adipisci consectetur impedit eveniet placeat dolore cumque minima omnis? Ad delectus quidem accusamus facere, nesciunt magni?",
+    title: "Triton Spinal Decompression",
+    image: "https://atriumphysiotherapy.com/images/tritonSpinal.png",
+    descriptionp1:
+      "Spinal Decompression is a non-surgical and drug-free answer for disc related problems of the lumbar or cervical spine. Many people across the country and around the world have found relief from the pain associated with herniated discs, bulging discs, facet syndrome, degenerative joint disease, pinched nerves, and other spinal afflictions from decompression therapy. Spinal Disc Decompression uses computer-aided technology to apply gentle, non-surgical decompression to your spine which increases circulation into the spinal discs and joints, thus helping to relieve the symptoms that cause pain and dysfunction.",
   },
   {
     id: 6,
-    title: "Pkikikik",
-    image: "../../assets/images/servicesimg/physiotherapy.jpg",
-    subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis culpa voluptatum laborum. Est officiis accusamus nesciunt corporis suscipit cum aspernatur ab culpa. Sapiente quae maxime, minus a magni animi quisquam!",
+    title: "Low Intensity Laser Therapy",
+    image: "https://atriumphysiotherapy.com/images/clinical_laser.jpg",
+    descriptionp1:
+      "Laser Therapy uses red and infrared light for the relief of pain, to accelerate healing and decrease inflammation. When the light source is placed against the skin, the photons penetrate several centimeters and get absorbed by the mitochondria, the energy producing part of a cell. This energy fuels many positive physiological responses resulting in the restoration of normal cell morphology and function. Laser Therapy has been successfully used to treat a broad range of medical conditions, including musculoskeletal problems, arthritis, sports injuries, post-surgical wounds, diabetic ulcers and dermatological conditions.",
+    descriptionp2:
+      "Unlike many pharmacological treatments that mask pain or only address the symptoms of disease, Laser Therapy treats the underlying condition or pathology to promote healing. This means that the treatments are effective and the benefits of Laser Therapy are long lasting.",
+    descriptionp3:
+      "The BioFlex Laser Therapy offers the highest clinical success rates with predictable, reliable and reproducible results",
   },
   {
     id: 7,
-    title: "Psvad",
-    image: "../../assets/images/servicesimg/physiotherapy.jpg",
-    subtitle:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis temporibus, ipsa in, nihil a aspernatur delectus ut laboriosam, similique facilis voluptate necessitatibus doloremque iusto mollitia illo iste ullam beatae aliquam!",
+    title: "K-Tape",
+    image: "https://atriumphysiotherapy.com/images/Ktape.jpeg",
+    descriptionp1:
+      "KT Tape® is applied along muscles, ligaments, and tendons (soft tissue) to provide a lightweight, external support that helps you remain active while recovering from injuries.*",
   },
   {
     id: 8,
-    title: "9PLOIKYHGRVF",
-    image: "../../assets/images/servicesimg/physiotherapy.jpg",
-    subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ut odio dignissimos earum neque voluptates modi suscipit natus? Eum vel odio doloribus, error atque consectetur praesentium. Dignissimos doloribus provident sint.",
+    title: "Massage Therapy",
+    image: "https://atriumphysiotherapy.com/images/massage_wide.jpg",
+    descriptionp1:
+      "Massage Therapy involves gentle hands-on manipulation of the soft tissues of your body. Therapeutic massage focuses on your muscles, connective tissue, tendons, ligaments and joints. Atrium Physiotherapy Registered Massage Therapists manipulate your muscles and other soft tissues of the body with varying degrees of pressure and movement. This helps to relieve pain and tension. If done soon enough after accidents involving trauma and injury, massage can greatly reduce the development of painful muscular patterning.",
+    descriptionp2:
+      "Massage Therapy is more than a period of relaxation, the therapeutic message helps diminish physical problems caused by stress, back pain, loss of energy, insomnia, etc. Massage can bring physical, mental and emotional changes. The benefits can vary from individual to individual.",
+  },
+  {
+    id: 9,
+    title: "Cupping Therapy",
+    image: "https://atriumphysiotherapy.com/images/cupping.jpeg",
+    descriptionp1:
+      "Hot cupping is an ancient form of alternative medicine in which a therapist puts special cups on your skin for a few minutes to create suction. People get it for many purposes, including to help with pain, inflammation, blood flow, relaxation and well-being, and as a type of deep-tissue massage.",
+  },
+  {
+    id: 10,
+    title: "Hot stone massage",
+    image: "https://atriumphysiotherapy.com/images/hotstone2.jpeg",
+    descriptionp1:
+      "Combining hot stone protocols with a full body massage provides a very healing and effective experience. The hot stones also expand blood vessels, which encourages blood flow throughout the body. The hot stones have a sedative effect that can relieve chronic pain, reduce stress and promote deep relaxation.",
+  },
+  {
+    id: 11,
+    title: "Custom Crafted Orthotics",
+    image: "https://atriumphysiotherapy.com/images/orthotics.jpg",
+    descriptionp1:
+      "An orthotic is a supportive device which is placed inside footwear, to change the mechanical function of the foot. It works dynamically during weight-bearing activities like walking, running and standing. Ideally, it should provide full, custom and corrected arch contact so that the foot works in a biomechanically correct way. At Atrium Physiotherapy we offer Custom Crafted Orthotics, calibrated to flex for your weight and foot type.",
+    descriptionp2:
+      "Wear your favorite shoes and boots while you relieve your foot, knee and lower back pain. Imagine a custom made orthotic to meet all of your lifestyle needs.",
+  },
+  {
+    id: 12,
+    title: "MVA (Motor Vehicle Accident)",
+    image: "https://atriumphysiotherapy.com/images/whiplash.jpg",
+    descriptionp1:
+      "A personal injury from auto accident trauma is common, but sometimes severe. An injury caused by Motor Vehicle Accident may result in lost work time, and cause difficulties in dealing with day-to-day responsibilities. After you've been injured in a crash, it's vital to get physiotherapy as quickly as possible. Many times, the effects of an auto injury are delayed or subtle. Seek medical advice as quickly as possible to ensure you are adequately treated and protected for the future.",
+    descriptionp2:
+      "It is easy to sustain neck trauma is when one car collides into another. The passenger in the automobile has their neck jerked back quickly resulting in an injury we know as whiplash.",
+  },
+  {
+    id: 13,
+    title: "WCB Alberta",
+    image: "https://atriumphysiotherapy.com/images/wcb.jpg",
+    descriptionp1:
+      "Workplace injuries are a common problem. Atrium Physiotherapy offers programs that provide workplace injury rehabilitation.",
+    descriptionp2:
+      "Atrium Physiotherapy focus on an active injury approach to promote healing, improve active range of motion, restore muscle strength and endurance and restore function, to facilitate early and safe return to work.",
+  },
+  {
+    id: 14,
+    title: "Private Insurance Claims",
+    image: "https://atriumphysiotherapy.com/images/ins.jpg",
+    descriptionp1:
+      "For individuals who has extended health insurance through their employer or their spouse's employer , we can do direct on-line billing for Private insurance companies such as Alberta Blue Cross ,and Great West Life.",
+    descriptionp2:
+      "If your plan is based on re-embarrassment , we do accept Debit, Visa and Master Card . We will provide you with an official receipt to submit to your private insurance.",
+    descriptionp3:
+      "our office manager will provide you with the necessary claim forms , and help you filling out the form to accelerate submitting your claim.",
+  },
+  {
+    id: 15,
+    title: "ART",
+    image:
+      "https://tarabrown.ca/wp-content/uploads/2013/10/Active-Release-Techniques.jpg",
+    descriptionp1:
+      "Active release technique (ART) treats your bodys soft tissue by combining manipulation and movement. The techniques been around for more than 30 years.",
+    descriptionp2:
+      "ART entails identifying, isolating, and targeting the affected area to break up scar tissue. This promotes blood flow and faster healing of injuries.",
+    descriptionp3:
+      "It was first used by Dr. P. Michael Leahy, a chiropractor, to treat soft tissue disorders in elite athletes and has since been used to treat millions of people.",
+  },
+  {
+    id: 16,
+    title: "Vestibular Rehabilitations",
+    image:
+      "https://tarabrown.ca/wp-content/uploads/2013/10/Active-Release-Techniques.jpg",
+    descriptionp1:
+      "Benign Paroxysmal Positional Vertigo (BPPV), an inner ear problem, is the most common cause of vertigo. We can normally correct the condition in 1-3 treatments.",
+    descriptionp2:
+      "Vestibular rehabilitation is a proven treatment for dizziness and balance problems and is highly effective for all age groups.",
+    descriptionp3:
+      "The vestibular system includes the parts of your inner ear that sense your head position and movement, the nerve pathways that carry that information to your brain, and the parts of the brain that process it.",
+    descriptionp4:
+      "The brain uses inputs from the vestibular system, visual system and your muscles and joints to maintain your balance, orientation, and the ability to see clearly when you move.",
+  },
+  {
+    id: 17,
+    title: "Pelvic Floor Physiotherapy",
+    image:
+      "https://www.activesolutionshealth.ca/wp-content/uploads/2020/07/pelvis-floor-physio.jpg",
+    descriptionp1:
+      "Pelvic floor physiotherapy help women rehabilitate their pelvic floor muscles. These muscles can be weakened by childbirth, surgery, heavy lifting, being overweight, constipation or menopause. The pelvic floor muscles support the womb (uterus), bladder and bowel (colon), forming a 'sling' from the pubic bone at the front to the tailbone at the back. If the muscles are weak, this can affect bladder and bowel control, resulting in incontinence (leakage) or prolapse.",
+    descriptionp2:
+      "For other women, their pelvic floor muscles may be overactive; that is, they can switch on involuntarily when they should be relaxing. These women need to learn to release their muscles. Overactivity can cause difficulty with having sex, emptying the bladder or bowels or using tampons.",
+    descriptionp3:
+      "This could include exercises, biofeedback (electronic monitoring), and manual treatment of the muscles. Advice about daily activities and fitness, as well as good bladder and bowel habits, will be given. Functional training of the pelvic floor gets your muscles back working effectively when you need them.",
+    descriptionp4:
+      "There is good evidence that shows that pelvic floor physiotherapy can help with urinary incontinence and prolapse and can lessen the effects of childbirth and menopause on the pelvic floor, says Jean Hailes pelvic floor physiotherapist Janetta Webb. A pelvic floor physiotherapist is an important part of the multi-disciplinary team managing bladder and bowel health, sexual difficulties and pelvic pain. Women should not have to put up with incontinence, pelvic pain or discomfort.",
   },
 ];
