c048edc3daf679ce6b65e4c82dea05fe09000b1b
Andy Caseres <andycaseres2@gmail.com>
2022-06-14 14:40:23 -0500

    iteration of cards services for active class active

--- src/Pages/Services_info/ServicesItems.jsx
+++ src/Pages/Services_info/ServicesItems.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./servicesitems.css";
 import "./services.css";
 
 export const ServicesItems = ({ item, setCurrent }) => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+    setCurrent(item.id);
+  };
+
   return (
-    <div onClick={() => setCurrent(item.id)} className="services">
-      <div className="servicesitems active">
+    <div
+      onClick={handleClick}
+      className={active ? "servicesitems active" : "servicesitems"}
+    >
+      <div>
         <h4 className="servicesitems-title">{item.title}</h4>
       </div>
     </div>
