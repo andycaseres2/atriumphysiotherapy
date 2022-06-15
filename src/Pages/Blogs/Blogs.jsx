7e17361661848fdb6dd33b4043560be086adbf06
Andy Caseres <andycaseres2@gmail.com>
2022-06-15 15:07:44 -0500

    correction grid cards blogs and add sidebarright category

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import SectionInfo from '../../Components/SectionInfo'
-import { Navbar } from '../../Components/Navbar'
-import SectionTitle from '../../Components/SectionTitle'
-import Footer from '../../Components/Footer'
-import { Grid } from '@chakra-ui/react'
-import { AddPost } from './AddPost'
-import { PostData } from './PostData'
-import { PostItem } from './PostItem'
+import React from "react";
+import SectionInfo from "../../Components/SectionInfo";
+import { Navbar } from "../../Components/Navbar";
+import SectionTitle from "../../Components/SectionTitle";
+import Footer from "../../Components/Footer";
+import { Grid } from "@chakra-ui/react";
+import { AddPost } from "./AddPost";
+import { PostData } from "./PostData";
+import { PostItem } from "./PostItem";
+import { SideBarRight } from "./SideBarRight";
+import "./blogs.css";
 
 const Blogs = () => {
   return (
@@ -14,15 +16,18 @@ const Blogs = () => {
       <SectionInfo />
       <Navbar />
       <SectionTitle title="Blogs" />
-      <Grid className="section-Blog">
-        {PostData.map((post) => (
-          <PostItem key={post.id} {...post} />
-        ))}
-      </Grid>
+      <div className="container-blogs">
+        <Grid className="section-Blog">
+          {PostData.map((post) => (
+            <PostItem key={post.id} {...post} />
+          ))}
+        </Grid>
+        <SideBarRight />
+      </div>
       <AddPost />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
---
+++ src/Pages/Blogs/SideBarRight.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./sidebarright.css";
+
+export const SideBarRight = () => {
+  return (
+    <div className="sidebarright">
+      <h2>Categorias</h2>
+      <ul>
+        <li>generico</li>
+        <li>generico</li>
+        <li>generico</li>
+        <li>generico</li>
+        <li>generico</li>
+      </ul>
+    </div>
+  );
+};
