48a6a3ece66ef00372564c313e09428e159ff6cf
Andy Caseres <andycaseres2@gmail.com>
2022-06-16 16:27:26 -0500

    add styles about home

--- src/Pages/Home/AboutHome.jsx
+++ src/Pages/Home/AboutHome.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Image, Stack, Box, Text } from "@chakra-ui/react";
 import imagestaffhome from "../../assets/images/homepageStaffPhoto.jpg";
-import "./AboutHome.css"
+import "./AboutHome.css";
 
 export const AboutHome = () => {
   return (
     <>
-      <Stack pl="150px" pr="150px">
-        <Stack>
+      <Stack justifyContent="center" alignItems="center">
+        <Stack width={1200} justifyContent="center" alignContent="center">
           <Box>
             <Image width={900} src={imagestaffhome} alt="Dan Abramov" />
           </Box>
@@ -30,42 +30,94 @@ export const AboutHome = () => {
             </Text>
           </Stack>
         </Stack>
-        <Stack className="stackContainer">   
-            <Stack textAlign="left">
-              {" "}
-              <img src="https://cdn-icons.flaticon.com/png/512/2251/premium/2251677.png?token=exp=1654528005~hmac=91d4e519e73e0ae3676c87ee08880393"width={40}/>
-              <Text  className="text">
+        <Stack
+          className="stackContainer"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Stack justifyContent="center" alignItems="center">
+            <Stack
+              flexDirection="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box border="3px solid blue" padding="10px">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/4005/4005394.png"
+                  width={35}
+                />
+              </Box>
+              <Text className="text">
                 We integrate customized physiotherapy treatment with injury
                 prevention techniques to ensure overall musculoskeletal well
                 being
               </Text>
             </Stack>
-            <Stack>
-            <img src="https://cdn-icons.flaticon.com/png/512/2251/premium/2251677.png?token=exp=1654528005~hmac=91d4e519e73e0ae3676c87ee08880393"width={40}/>
-              <Text  className="text">
+            <Stack
+              flexDirection="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box border="3px solid blue" padding="10px">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/4005/4005394.png"
+                  width={35}
+                />
+              </Box>
+              <Text className="text">
                 Physiotherapy modalities, Low Intensity Laser, deep tissue
                 massage, active resistive strengthening techniques,
                 proprioceptive rehabilitation and therapeutic exercise are just
                 some of the scientifically based solutions we apply to our
                 patients during their treatment plan.
               </Text>
-            </Stack>      
-            <Stack textAlign="left">
-            <img src="https://cdn-icons.flaticon.com/png/512/2251/premium/2251677.png?token=exp=1654528005~hmac=91d4e519e73e0ae3676c87ee08880393"width={40}/>
+            </Stack>
+          </Stack>
+          <Stack>
+            <Stack
+              textAlign="left"
+              flexDirection="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box border="3px solid blue" padding="10px">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/4005/4005394.png"
+                  width={35}
+                />
+              </Box>
               <Text className="text">
                 Our Physiotherapist combine a balanced approach after properly
                 diagnosing the problem.
               </Text>
             </Stack>
-            <Stack>
-            <img src="https://cdn-icons.flaticon.com/png/512/2251/premium/2251677.png?token=exp=1654528005~hmac=91d4e519e73e0ae3676c87ee08880393"width={40}/>
+            <Stack
+              flexDirection="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box border="3px solid blue" padding="10px">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/4005/4005394.png"
+                  width={35}
+                />
+              </Box>
               <Text className="text">
                 We believe that in order to maintain a pain free body it is
                 necessary to understand a person's specific injury and prevent
                 situations that might exacerbate or aggravate a condition.
               </Text>
             </Stack>
+          </Stack>
         </Stack>
+        <Text width="1200px" p="40px 0 40px 0" textAlign="left">
+          At Atrium Physiotherapy we strive to educate our patients about their
+          condition and collaborate in developing a realistic treatment plan.
+          Our teams of Physiotherapists and Massage Therapist have exceptional
+          credentials and some 20 years of experience with handling all types of
+          musculoskeletal , and neurological issues.
+        </Text>
       </Stack>
     </>
   );
