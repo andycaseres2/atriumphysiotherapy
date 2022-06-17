4d9a1006636c90124c0bba7ae18afd486591512d
Damian <58891112+damianroiz@users.noreply.github.com>
2022-06-17 09:30:20 -0400

    added content to services_data file

--- src/Pages/Services_info/services_data.js
+++ src/Pages/Services_info/services_data.js
@@ -3,8 +3,10 @@ export const servicesdata = [
     id: 1,
     title: 'Physiotherapy',
     image: 'https://atriumphysiotherapy.com/images/stretching.jpg',
-    description:
-      'Our physiotherapists are highly-educated experts in physical function, movement and mobility. They have advanced knowledge of how the human body moves and what stops it moving and use specialized hands-on treatment to restore, maintain and maximize optimal function and quality of life.',
+    description: [
+      "Our physiotherapists are highly-educated experts in physical function, movement and mobility. They have advanced knowledge of how the human body moves and what stops it moving and use specialized hands-on treatment to restore, maintain and maximize optimal function and quality of life.Our physiotherapists assists, diagnose and treat physical symptoms and limited movement caused by injury, aging, disability, or medical condition. They help patients understand what is causing their condition, and work with them to restore, maintain and maximize movement, flexibility and physical independence., Our physiotherapists develop customized treatment plans that help patients take back control. They teach patients how to restore, maintain and/or maximize movement, reduce pain, and manage any chronic symptoms., Our physiotherapists excel in the treatment of many conditions: back pain/injury, whiplash, pregnancy-related muscle/joint issues, urinary incontinence and vertigo to name a few. They also help patients manage symptoms of chronic conditions like arthritis and chronic pain., Our physiotherapists help patients who may have otherwise tried temporary (e.g., prescription drugs) or more invasive methods (e.g., surgery) to manage their condition. They use individualized therapeutic exercise, manual therapy and other treatment techniques., Through physiotherapy, many patients are able to recover unrestricted movement which promotes the kind of physical independence necessary for a normal lifestyle and work. When it comes to assessing and treating people with movement problems, the services of a physiotherapist are often essential."
+    ]
+      
   },
   {
     id: 2,
