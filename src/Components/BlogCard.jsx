1d53f87d8e78ea43aca4c4fcb5ca3c58b1b7fef3
Damian Roiz <damianx989@gmail.com>
2022-07-04 23:15:05 -0400

    blog cards styling

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 
+
 export default function BlogCard(props) {
     return (
         <div className="blogcard">
           <img src={props.image}/>
           <div className="blogcard-info">
-            <h2 className="blogcard-title">{props.title}</h2>
-            <p className="blogcard-date">{props.date}</p>
-            <p className="description">{props.description}</p>
+            <h2 className="title">{props.title}</h2>
+            <p className="date">{props.date}</p>
+            <p className="desc">{props.description}</p>
             <button className="blogcard-btn"><a href="#">Read More</a></button>
           </div>
         </div>
