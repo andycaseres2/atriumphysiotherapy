f633eeb8f1294eb2fc125664f338b4d3a2e7ae61
Damian Roiz <damianx989@gmail.com>
2022-07-04 20:03:58 -0400

    blogcard

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function BlogCard(props) {
-    retrun (
+    return (
         <div className="blogcard">
           <img src={item.image}/>
           <div className="blogcard-info">
